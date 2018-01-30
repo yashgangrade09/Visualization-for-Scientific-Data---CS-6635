b70ef81d4a44ae5f4f6ec78c4289ad1442f4e695
Yash <yashgangrade09@gmail.com>
2018-01-29 17:26:38 -0700

    Part 2 Q3 Completed

--- Part2Q3.py
+++ Part2Q3.py
@@ -18,7 +18,7 @@ for row in csvreader:
     data.append(row)
     year = np.append(year, row[0])
     date_of_month[int(row[2])] = date_of_month[int(row[2])] + int(row[4])
-print(data[4][2])
+
 ## Finding the day with maximum birthdays
 max_value = max(date_of_month.values())
 max_keys = [k for k, v in date_of_month.items() if v == max_value]
@@ -31,9 +31,18 @@ print("The day with lowest number of births is: %s and the birth count is %d"% (
 
 ## Counting the number of birthdays on Friday the 13th
 
-friday13 = sum(int(p[4]) for p in data if p[2] == '13' and p[3] == '5')
+f_13 = sum(int(p[4]) for p in data if p[2] == '13' and p[3] == '5')
 #print(friday13)
-print("Number of Birthdays on Friday the 13th: %d" % friday13)
+print("Number of Birthdays on Friday the 13th: %d" % f_13)
+
+counter_summer = 0
+counter_winter = 0
 
 ## Birthdays in Summer (May (Month 05)- August (Month 08) of every year)
+counter_summer = sum(int(row[4]) for row in data if row[1] == '5' or row[1] == '6' or row[1] == '7' or row[1] == '8')
 
+## Birthdays in Winter (November (Month 11)- February (Month 02) of every year)
+counter_winter = sum(int(row[4]) for row in data if row[1] == '1' or row[1] == '2' or row[1] == '11' or row[1] == '12')
+
+print("Number of Birthdays in Winter %d and number of Birthdays in Summers are %d. "
+      "It can be concluded that winters have less births than summers." % (counter_winter, counter_summer))
