95bebf196d9e69708922caea8f77e0d6255bf06f
Yash <yashgangrade09@gmail.com>
2018-01-29 22:10:26 -0700

    Working on part 4

---
+++ Part2Q4.py
@@ -0,0 +1,16 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import us
+
+'''
+f = open("Dataset/p2q4d1.csv")
+csvreader = csv.reader(f)
+
+#skip the first header information line
+next(csvreader)
+data = []
+state = []
+for row in csvreader:
+    state.append(row[5])
+'''
