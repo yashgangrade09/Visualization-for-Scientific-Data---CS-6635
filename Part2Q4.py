5a7b3b6c85acd021d6fbbd3a63f8e93137a438e8
Yash <yashgangrade09@gmail.com>
2018-01-31 23:50:31 -0700

    Q4 Part 2 Finally Complete

--- Part2Q4.py
+++ Part2Q4.py
@@ -96,8 +96,6 @@ def piechart_d1():
     dom = dom.astype(int)
     foreign = foreign.astype(int)
 
-    # plt.hist(tot)
-    # fig1, ax1 = plt.subplots()
     plt.pie(tot, labels=year, autopct='%1.1f%%',startangle=90)
     plt.axis('equal')
     plt.title("Pie Chart depicting total sales every year from 1974 - 2013")
@@ -163,12 +161,116 @@ def pieplot_d2():
         else:
             c3 += int(row[5].replace(",", ""))
             recallclass['2'] = c3
-        counter = True
-        if counter == True:
-            plt.pie([float(v) for v in recallclass.values()], labels = [k for k in recallclass.keys()], autopct='%1.1f%%',startangle=90 )
-            plt.axis('equal')
-            counter = False
-        plt.show()
+    plt.pie([float(v) for v in recallclass.values()], labels = [k for k in recallclass.keys()], autopct='%1.1f%%',startangle=90 )
+    plt.axis('equal')
+    plt.show()
+
+def scatterplot_d2():
+    f = open("Dataset/FSIS-Recall-Summary-2014.csv")
+    csvreader = csv.reader(f)
+
+    for i in range(8):
+        next(csvreader)
+    data = []
+    for row in csvreader:
+        data.append(row)
+    # print(data)
+
+    pounds = []
+    recallclass = {'0': 0, '1': 0, '2': 0}
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    for row in data:
+        # print(row[5].replace(",",""))
+        if row[2] == 'I':
+            c1 += int(row[5].replace(",", ""))
+            recallclass['0'] = c1
+        if row[2] == "II":
+            c2 += int(row[5].replace(",", ""))
+            recallclass['1'] = c2
+        else:
+            c3 += int(row[5].replace(",", ""))
+            recallclass['2'] = c3
+
+    x = [k for k in recallclass.keys()]
+    y = [k for k in recallclass.values()]
+    colors = list("rgbcmyk")
+    plt.scatter(x,y)
+    plt.show()
+
+
+
+###### Dataset 3 - Time Series  of National Population Estimates by U.S. Census Bureau######
+
+##### Plot type - Bar Plot ######
+
+def barplot_d3():
+    f = open("Dataset/National_Population_Estimates.csv")
+    csvreader = csv.reader(f)
+    # skip the first two header information line
+    next(csvreader)
+    next(csvreader)
+    year = np.array([])
+    population = np.array([])
+    for row in csvreader:
+        year = np.append(year, row[0])
+        population = np.append(population, row[1])
+    year = year.astype(int)
+    population = population.astype(int)
+
+    ## Plot the bar plots
+    fig = plt.figure(figsize=(80, 20))
+    ax = fig.add_subplot(111)
+    r1 = ax.bar(year, population, width=0.4, color='b', align='center')
+    ax.set_xticks(year)
+    ax.autoscale(tight=True)
+    plt.xlabel("Year")
+    plt.ylabel("Total Population Estimate as of July 1 ( x 1000)")
+    plt.title("Time Series  of National Population Estimates by U.S. Census Bureau (from 2000 to 2015)")
+    plt.show()
+
+def lineplot_d3():
+    f = open("Dataset/National_Population_Estimates.csv")
+    csvreader = csv.reader(f)
+    # skip the first two header information line
+    next(csvreader)
+    next(csvreader)
+    year = np.array([])
+    population = np.array([])
+    for row in csvreader:
+        year = np.append(year, row[0])
+        population = np.append(population, row[1])
+    year = year.astype(int)
+    population = population.astype(int)
+
+    r1 = plt.plot(year, population, 'b--')
+    plt.autoscale(tight=True)
+    plt.xticks(year)
+    plt.xlabel("Year")
+    plt.ylabel("Total Population Estimate as of July 1 ( x 1000)")
+    plt.title("Time Series of National Population Estimates by U.S. Census Bureau (from 2000 to 2015)")
+    plt.grid()
+    plt.show()
+
+def piechart_d3():
+    f = open("Dataset/National_Population_Estimates.csv")
+    csvreader = csv.reader(f)
+    # skip the first two header information line
+    next(csvreader)
+    next(csvreader)
+    year = np.array([])
+    population = np.array([])
+    for row in csvreader:
+        year = np.append(year, row[0])
+        population = np.append(population, row[1])
+    year = year.astype(int)
+    population = population.astype(int)
+
+    plt.pie(population, labels=year, autopct='%1.1f%%', startangle=90)
+    plt.axis('equal')
+    plt.title("Pie Chart depicting Time Series of National Population Estimates by U.S. Census Bureau (from 2000 to 2015)")
+    plt.show()
 
 # Run for Dataset 1
 # lineplot_d1()
@@ -177,5 +279,10 @@ def pieplot_d2():
 
 # Run for Dataset 2
 # barplot_d2()
-pieplot_d2()
+# pieplot_d2()
+# scatterplot_d2()
 
+# Run for Dataset 3
+# barplot_d3()
+# lineplot_d3()
+piechart_d3()
